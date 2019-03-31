d76be5f49a8dc2df6de1aa680b5d7a335e7801e9
hww <valery.hww@gmail.com>
2019-03-31 09:06:17 +0300

    images

---
+++ rom/rainbow-font-converter.rb
@@ -0,0 +1,26 @@
+#!/bn/ruby
+
+file = File.open("rainbow-font.BIN", "rb")
+contents = file.read.bytes
+puts "RAINBOW COMPUTER"
+puts "FONT"
+puts "SIZE: #{contents.size}"
+
+for y in 0..15 do
+	for l in 0..7 do
+		for x in 0..15 do
+			addr = (x * 16 + y) * 8 + l
+			data = contents[addr]
+                        for bit in [128,64,32,16,8,4,2,1] do
+                          if data & bit > 0
+                            print "X"
+                          else
+                            print "."
+                          end
+                        end
+			print " "
+		end
+		print "\n"
+	end
+        print "\n"
+end
